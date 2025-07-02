3499106d311401271a4e878779e5c250e2dad4e8
Lwant <jaimain671@gmail.com>
2025-07-02 14:00:17 +0700

    implement:finished login and logout feature

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -2,4 +2,10 @@ import { config } from "dotenv";
 
 config();
 
-export const { FRONTEND_URL, PORT, JWT_SECRET } = process.env;
+export const {
+  FRONTEND_URL,
+  PORT,
+  JWT_REFRESH_SECRET,
+  JWT_ACCESS_SECRET,
+  NODE_ENV,
+} = process.env;
---
+++ src/util/generateToken.util.ts
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+import { JWT_ACCESS_SECRET, JWT_REFRESH_SECRET } from "../config/env.config";
+
+export const generateRefreshToken = (userId: string): string => {
+  return jwt.sign({ userId }, JWT_REFRESH_SECRET as string, {
+    expiresIn: "7d",
+  });
+};
+
+export const generateAccessToken = (userId: string): string => {
+  return jwt.sign({ userId }, JWT_ACCESS_SECRET as string, {
+    expiresIn: "15m",
+  });
+};

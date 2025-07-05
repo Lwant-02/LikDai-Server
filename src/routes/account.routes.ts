56aaab0291b3f429c0407679b93e70fba8ef31fd
Lwant <jaimain671@gmail.com>
2025-07-05 23:50:24 +0700

    implement update username, password and delete account features

--- src/routes/account.routes.ts
+++ src/routes/account.routes.ts
@@ -1,7 +1,15 @@
 import { Router } from "express";
-import { getMe } from "../controller/account.controllers";
+import {
+  deleteAccount,
+  getMe,
+  updatePassword,
+  updateUsername,
+} from "../controller/account.controllers";
 import { verifyAccessToken } from "../middleware/auth.middleware";
 
 export const accountRouter: Router = Router();
 
 accountRouter.get("/me", verifyAccessToken, getMe);
+accountRouter.patch("/update-username", verifyAccessToken, updateUsername);
+accountRouter.patch("/update-password", verifyAccessToken, updatePassword);
+accountRouter.delete("/delete-account", verifyAccessToken, deleteAccount);
